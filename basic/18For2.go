2def96edb3819dc87438cf7ab390b5184f59a0f4
azole <azolelai@gmail.com>
2013-12-08 23:33:33 +0800

    reverse for statement in 18For2.go

--- basic/18For2.go
+++ basic/18For2.go
@@ -1,7 +1,5 @@
 // 跟 C 或 Java 一樣，可以讓 for 的第一個跟最後一個敘述為空
 // 可以寫成 for ; sum < 1000; {
-// 但 goSublime太聰明了，一直幫我把前後兩個 ; 消掉
-// 變成像範例 19 那樣的 while 的形式
 
 package main
 
@@ -9,7 +7,7 @@ import "fmt"
 
 func main() {
 	sum := 1
-	for sum < 1000 {
+	for ;sum < 1000; {
 		sum += sum
 	}
 	fmt.Println(sum)
