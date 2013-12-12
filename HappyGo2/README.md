1ed2161b8488eadb68bdccfc2700bc352d210223
azole <azolelai@gmail.com>
2013-12-12 20:17:39 +0800

    add Happy go language part2 exercise

---
+++ HappyGo2/README.md
@@ -0,0 +1,10 @@
+Happy go Language Part II
+=========================
+
+參加 <a href="http://www.openfoundry.org/tw/activities/details/409-happy-go-language-part2" target="_blank">Happy go Language Part II</a>，在這個課程中所做的練習。
+
+講師：c9s
+
+投影片：
+1. <a href="http://www.slideshare.net/c9s/happy-gopart1" target="_blank">Part I</a>
+2. <a href="http://www.slideshare.net/c9s/happy-go-programming-part-2" target="_blank">Part II</a>
---
+++ HappyGo2/wiki.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"fmt"
+	"html/template"
+	"io/ioutil"
+	"net/http"
+)
+
+type Page struct {
+	Title string
+	Body  []byte
+}
+
+func (p *Page) save() error {
+	filename := p.Title + ".txt"
+	return ioutil.WriteFile(filename, p.Body, 0600)
+}
+
+func loadPage(title string) (*Page, error) {
+	filename := title + ".txt"
+	body, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	// r.URL.Path --> 抓第一個斜線後的值，包括斜線
+	// http://localhost:4000/azole/test/ha
+	// r.URL.Path = /azole/test/ha
+	// r.URL.Path[1:] --> azole/test/ha
+
+	fmt.Fprintf(w, "Hi there, I love %s!", r.URL.Path[1:])
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/view/"):]
+	p, err := loadPage(title)
+	if err != nil {
+		fmt.Fprintf(w, "Error: %s", err)
+		return
+	}
+	fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+
+	// if p, err := loadPage(title); err == nil{
+	//   fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+	// }
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/edit/"):]
+	p, err := loadPage(title)
+	if err != nil {
+		p = &Page{Title: title}
+	}
+	t, _ := template.ParseFiles("edit.html")
+	t.Execute(w, p)
+}
+
+func saveHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/save/"):]
+	body := r.FormValue("body")
+	p := &Page{Title: title, Body: []byte(body)}
+	p.save()
+	http.Redirect(w, r, "/view/"+title, http.StatusFound)
+}
+
+func main() {
+	p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
+	p1.save()
+	p2, _ := loadPage("TestPage")
+	fmt.Println(string(p2.Body))
+
+	// 由最長的開始比對，比到 match 為止
+	http.HandleFunc("/", handler)
+	http.HandleFunc("/view/", viewHandler)
+	http.HandleFunc("/edit/", editHandler)
+	http.HandleFunc("/save/", saveHandler)
+	http.ListenAndServe(":4000", nil)
+}
