70486a37a4cec3e1138651131b5881ad4c158e57
azole <azolelai@gmail.com>
2013-12-12 16:14:30 +0800

    add Interface examples in basic

---
+++ basic/53Interfaces.go
@@ -0,0 +1,43 @@
+// 介面是一組方法的定義
+// 一個介面類型的值會存放任何想要被實作的方法
+
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type Abser interface {
+	Abs() float64
+}
+
+func main() {
+	var a Abser
+	f := MyFloat(-math.Sqrt2)
+	v := Vertex{3, 4}
+
+	a = f  // a MyFloat implements Abser
+	a = &v // a *Vertex implements Abser
+	a = v  // a Vertex, does NOT implements Abser
+	// 這行會有錯誤發生
+
+	fmt.Println(a.Abs())
+}
+
+type MyFloat float64
+
+func (f MyFloat) Abs() float64 {
+	if f < 0 {
+		return float64(-f)
+	}
+	return float64(f)
+}
+
+type Vertex struct {
+	X, Y float64
+}
+
+func (v *Vertex) Abs() float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
---
+++ basic/54InterfacesImplicity.go
@@ -0,0 +1,36 @@
+// 一個類型藉由實作那些方法來實作一個介面
+// 沒有顯式聲明的必要
+//
+// 隱式聲明解耦 (decouple) 了實作的 package 跟那些定義了 interface 的 package
+// 讓這兩者不互相依賴
+// 這樣也鼓勵了明確的介面定義，因為你不用再找到每個實作，然後用新的 interface 名稱標簽它
+//
+// Package io 定義了 Reader 跟 Writer，你不用這樣做
+
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+type Reader interface {
+	Read(b []byte) (n int, err error)
+}
+
+type Writer interface {
+	Write(b []byte) (n int, err error)
+}
+
+type ReadWriter interface {
+	Reader
+	Writer
+}
+
+func main() {
+	var w Writer
+	// os.Stdout implements Writer
+	w = os.Stdout
+
+	fmt.Fprintf(w, "hello, writer\n")
+}
