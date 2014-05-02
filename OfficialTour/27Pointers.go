b85f929a452879e3775eb6492f5b170215897fd4
azole <azolelai@gmail.com>
2014-05-02 18:49:04 +0800

    add more introduction

--- OfficialTour/27Pointers.go
+++ OfficialTour/27Pointers.go
@@ -1,6 +1,9 @@
 // go 有 pointers，但沒有指標的運算
 // struct 的欄位可以透過 struct 指標來存取
 // The indirection through the pointer is transparent. -> 這句不知道什麼意思
+// 我猜是指，透過 pointer 的存取，會影響到原本的值
+// 例如以下範例，修改了 q，但 p 也會被影響
+// 而 z 不是透過 pointer 存取，所以 z 的修改不會影響到 p
 
 package main
 
@@ -14,6 +17,9 @@ type Vertex struct {
 func main() {
 	p := Vertex{1, 2}
 	q := &p
+	z := p
 	q.X = 1e9
-	fmt.Println(p)
+	z.Y = 55
+	fmt.Println(p) // {1000000000 2}
+	fmt.Println(z) // {1 55}
 }
